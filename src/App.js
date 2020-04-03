ae2cf319218f5d9972d701c925daf624c0f8f779
Mehdi RAFENNE <mehdi.rafenne@gmail.com>
2020-04-03 13:10:43 +0300

    Middleware thunk + reducer authedUser created + App connect()ed : App shows the authedUser's id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { handleInitialData } from './actions/shared'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.props.authedUser }
+      </div>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser: authedUser
+  };
+}
+
+export default connect(mapStateToProps)(App);
